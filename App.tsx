230f32c9db552f861bede076ccd31866bf3bc603
YH803118 <dudghks1045@naver.com>
2023-01-09 15:24:40 +0900

    npm run dev //ok

--- App.tsx
+++ App.tsx
@@ -1,8 +1,17 @@
 import * as React from "react";
 import { FC } from "react";
+import Body from "./component/Body";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
 
 const App: FC = () => {
-  return <>TEST</>;
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
---
+++ component/Body.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Body = () => {
+  return <div>Body</div>;
+};
+
+export default Body;
