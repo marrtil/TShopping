f81a39f6e9b3a19b0863d1a475fd39f6b73c19aa
YH803118 <dudghks1045@naver.com>
2023-01-09 15:08:09 +0900

    client.tsx

--- client.tsx
+++ client.tsx
@@ -1,10 +1,18 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <>
+//     <App />
+//   </>,
+//   document.querySelector("#root")
+// );
+const rootEl = document.getElementById("root");
+if (!rootEl) throw new Error("Failed to find the root element");
+const root = ReactDOM.createRoot(rootEl);
+root.render(
   <>
     <App />
-  </>,
-  document.querySelector("#root")
+  </>
 );
