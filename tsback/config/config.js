0f96ba81062162d40453b7aa7fecbd56e66451d9
YH803118 <dudghks1045@naver.com>
2023-02-16 16:46:14 +0900

    db config retry

--- tsback/config/config.js
+++ tsback/config/config.js
@@ -3,25 +3,25 @@ exports.__esModule = true;
 var dotenv = require("dotenv");
 dotenv.config();
 module.exports = {
-    development: {
-        username: "root",
-        password: process.env.DB_PASSWORD,
-        database: "tshop-dev",
-        host: "127.0.0.1",
-        dialect: "mysql"
-    },
-    test: {
-        username: "root",
-        password: process.env.DB_PASSWORD,
-        database: "tshop-test",
-        host: "127.0.0.1",
-        dialect: "mysql"
-    },
-    production: {
-        username: "root",
-        password: process.env.DB_PASSWORD,
-        database: "tshop-pro",
-        host: "127.0.0.1",
-        dialect: "mysql"
-    }
+  development: {
+    username: "root",
+    password: process.env.DB_PASSWORD,
+    database: "tshop-dev",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: process.env.DB_PASSWORD,
+    database: "tshop-test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    host: process.env.HOST,
+    dialect: "mysql",
+  },
 };
