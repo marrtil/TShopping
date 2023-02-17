25e32c3f98908933542c367c613d8a6d60d5affd
yunjiseob <marrtil@naver.com>
2023-02-17 12:24:44 +0900

    일단 footer 가져옴

--- component/Footer.tsx
+++ component/Footer.tsx
@@ -1,12 +1,73 @@
 import * as React from "react";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import StyledFooter from "./styles/StyledFooter";
+function Footer() {
+  const ballonEffect1 = () => {
+    const ballon1 = document.querySelectorAll(".ballon")[0] as HTMLDivElement;
+    const ballon2 = document.querySelectorAll(".ballon2")[0] as HTMLDivElement;
+    ballon2.style.display = "none";
 
-const Footer = () => {
+    ballon1.style.display = "block";
+  };
+
+  const ballonEffect2 = () => {
+    const ballon1 = document.querySelectorAll(".ballon")[0] as HTMLDivElement;
+    const ballon2 = document.querySelectorAll(".ballon2")[0] as HTMLDivElement;
+    ballon1.style.display = "none";
+
+    ballon2.style.display = "block";
+  };
+
+  const disBallon = () => {
+    const ballon1 = document.querySelectorAll(".ballon")[0] as HTMLDivElement;
+    const ballon2 = document.querySelectorAll(".ballon2")[0] as HTMLDivElement;
+    ballon1.style.display = "none";
+
+    ballon2.style.display = "none";
+  };
+
   return (
     <StyledFooter>
-      footer - 대충 연락처나 정보 이건 항상애매하네 1:1문의? 공지사항?
+      <div id="container" onMouseUp={disBallon}>
+        <div className="container2">
+          <div className="icon">
+            <a href="https://github.com/marrtil/TShopping">
+              <FontAwesomeIcon icon={faGithub} color="black" />
+            </a>
+            <p className="icon-font">깃허브 주소</p>
+          </div>
+          <div className="icon">
+            <p className="ballon">
+              윤지섭:marrtil@naver.com
+              <br />
+              김영환:dudghks1045@naver.com
+            </p>
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              color="#00cfd5"
+              onClick={ballonEffect1}
+            />
+            <p className="icon-font">이메일</p>
+          </div>
+          <div className="icon">
+            <p className="ballon2">
+              윤지섭:010-8341-2331
+              <br />
+              김영환:010-2948-2645
+            </p>
+            <FontAwesomeIcon
+              icon={faPhone}
+              color="black"
+              onClick={ballonEffect2}
+            />
+            <p className="icon-font">전화</p>
+          </div>
+        </div>
+      </div>
     </StyledFooter>
   );
-};
+}
 
 export default Footer;
--- component/styles/StyledFooter.tsx
+++ component/styles/StyledFooter.tsx
@@ -1,7 +1,86 @@
 import styled from "styled-components";
 
 const StyledFooter = styled.div`
-  background-color: rgb(150, 150, 150);
+  #container {
+    background-color: rgb(225, 226, 227);
+    width: 100%;
+    height: 150px;
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    position: relative;
+  }
+
+  .container2 {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .icon {
+    font-size: 30px;
+    display: flex;
+    margin-right: 15px;
+    flex-direction: column;
+    text-align: center;
+    vertical-align: middle;
+  }
+
+  .icon p {
+    font-size: 10px;
+  }
+
+  #helper {
+    height: 100%;
+  }
+
+  .ballon {
+    display: none;
+    position: absolute;
+    width: 205px;
+    height: 16px;
+    left: 550px;
+    bottom: 100px;
+    background: #484848;
+    color: white;
+    border-radius: 5px;
+    padding: 12px 12.8px;
+  }
+
+  .ballon:after {
+    border-top: 10px solid #484848;
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    border-bottom: 0px solid transparent;
+    content: "";
+    position: absolute;
+    top: 40px;
+    left: 160px;
+  }
+
+  .ballon2 {
+    display: none;
+    position: absolute;
+    width: 205px;
+    height: 16px;
+    left: 590px;
+    bottom: 100px;
+    background: #484848;
+    color: white;
+    border-radius: 5px;
+    padding: 12px 12.8px;
+  }
+
+  .ballon2:after {
+    border-top: 10px solid #484848;
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    border-bottom: 0px solid transparent;
+    content: "";
+    position: absolute;
+    top: 40px;
+    left: 160px;
+  }
 `;
 
 export default StyledFooter;
